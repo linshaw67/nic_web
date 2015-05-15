34b82a1d26cc3b0dcc99f2fe90ff5ae3ea730f15
linshaw <liusq1205@gmail.com>
2015-05-15 14:47:12 -0400

    cat update

--- importadorabacco/src/main/webapp/WEB-INF/views/scripts/main.js
+++ importadorabacco/src/main/webapp/WEB-INF/views/scripts/main.js
@@ -72,6 +72,6 @@ $(document).ready(function(){
     $("#home2 .img").height($("#home2 .leftwrap").height() - 40);
     $("#home2 .img").width(600 * $("#home2 .img").height() / 447);
     $(".categories a").click(function(){
-            location = "./product?catId=" + $(this).parent().data("catId");
+            location = "./product?catId=" + $(this).parent().data("catid");
     });
 })
--- importadorabacco/src/main/webapp/WEB-INF/views/scripts/product.js
+++ importadorabacco/src/main/webapp/WEB-INF/views/scripts/product.js
@@ -1,14 +1,15 @@
 function createCat(cat){
     var i, $newCat, $line, line_count, $newProduct;
     $newCat = $("<div class='category'>\
-                    <div class='category-name'></div>\
+                    <div class='category-name'><div></div></div>\
                     <div class='products'></div>\
                 </div>");
-    $newCat.find(".category-name").text(cat['catName']);
+    $newCat.find(".category-name div").text(cat['catName']);
     $line = $("<ul></ul>");
     line_count = 0;
     for (i=0;i<cat['products'].length;i++){
         if (line_count == 6) {
+            $line.append("<div class='clear'></div>");
             $newCat.find(".products").append($line);
             $line = $("<ul></ul>");
             line_count = 0;
@@ -19,7 +20,9 @@ function createCat(cat){
         $line.append($newProduct);
         line_count++;
     };
+    $line.append("<div class='clear'></div>");
     $newCat.find(".products").append($line);
+    $("#pcontent").append($newCat);
 };
 $(document).ready(function(){
     var defaultCatId = location.href.slice(location.href.indexOf('=')+1, location.href.length);
@@ -32,21 +35,19 @@ $(document).ready(function(){
         method: "get",
         url: "./product/get?catId="+defaultCatId,
         dataType: "json",
-        statusCode: {
-            0: function(data){
-                if (defaultCatId == 0){
-                    pselectionText = "ALL PRODUCTS";
+        success:function(data){
+                    if (defaultCatId == 0){
+                        pselectionText = "ALL PRODUCTS";
+                    }
+                    else{
+                        pselectionText = data["data"][0]['catName'];
+                    }
+                    $("#pcontent .panel .pselection").text(pselectionText);
+                    $("#pcontent .panel").show();
+                    for (i = 0; i < data["data"].length; i++) {
+                        createCat(data["data"][i]);
+                    }
                 }
-                else{
-                    pselectionText = data[0]['catName'];
-                }
-                $("#pcontent .panel .pselection").text(pselectionText);
-                $("#pcontent .panel").show();
-                for (i = 0; i < data.length; i++) {
-                    createCat(data[i]);
-                }
-            }
-        }
     });
     $(".arrow").click(function(){
         //$(this).animate({"left":  $("#pcontent .panel .categories").width() + "px"}, "300");
@@ -58,22 +59,21 @@ $(document).ready(function(){
         });
         $("#pcontent .panel .categories").fadeTo(800, 1);
     });
-    $(".category a").click(function(){
+    $("#pcontent .panel .categories a").click(function(){
         var $r = $(this);
         $.ajax({
             method: "get",
-            url: "./product/get?catId="+$(this).parent().data("catId"),
+            url: "./product/get?catId="+$(this).parent().data("catid"),
             dataType: "json",
-            statusCode: {
-                0: function(data){
-                    $("#pcontent .panel .pselection").show().text($r.text());
-                    $(".arrow").show();
-                    $("#pcontent .panel .categories").fadeOut();
-                    for (i = 0; i < data.length; i++) {
-                        createCat(data[i]);
-                    }
-                }
-            }
+            success: function(data){
+                        $("#pcontent .panel .pselection").fadeIn().text($r.text());
+                        $(".arrow").fadeIn();
+                        $("#pcontent .panel .categories").hide();
+                        $("#pcontent .category").remove();
+                        for (i = 0; i < data["data"].length; i++) {
+                            createCat(data["data"][i]);
+                        }
+                     }
         });
     });
 });
